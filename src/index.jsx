0373637a0de7b0bd2e932c49fe0bf613b3a8c555
Vladislav Kireev <kireew.vladislaw@gmail.com>
2023-01-31 20:02:05 +0300

    initial: set up dependencies, added general folders

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './components/app/App';
+import './pages/index';
+// import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <React.StrictMode>
+    {/* <Provider store={store()} > */}
+    <App />
+    {/* </Provider> */}
+  </React.StrictMode>
+);
